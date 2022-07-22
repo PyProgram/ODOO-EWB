cfef4a82945a124dfcc1b9b24efc3eeacb277c34
OdooPy <85791075+PyProgram@users.noreply.github.com>
2022-07-22 12:29:06 +0530

    Security

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,8 @@
     "version" : "1.0",
     "description" : "This module helps to generate eway bill against Invoices",
     "data" : [  'security/ir.access.model.csv',
-                'views/invoice_menu.xml' ,
-                'views/invoice_views.xml'],
+                'views/invoice_views.xml',
+                'views/invoice_menu.xml' 
+             ],
     "application" : True
 }
